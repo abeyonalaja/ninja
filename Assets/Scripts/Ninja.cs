8a5f17fa32a1e0d095cb5a0df1713d2b5ca080ca
Abideen Onalaja <abey.onalaja@gmail.com>
2014-12-04 01:36:46 +0000

    Added a Script for the ninja charcter.

---
+++ Assets/Scripts/Ninja.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ninja : MonoBehaviour {
+
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate () {
+		MoveCharacter ();
+	}
+
+	void MoveCharacter () {
+
+	}
+}
